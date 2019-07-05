f329915770ebd6576fd364bb30d9258b6e76e562
Heng Li <lh3@me.com>
2019-07-05 11:23:22 -0400

    subset was not working properly

--- gfa-util.c
+++ gfa-util.c
@@ -6,7 +6,10 @@ void gfa_arc_rm(gfa_t *g);
 void gfa_sub(gfa_t *g, int n, char *const* seg, int step)
 {
 	int32_t i;
+	int8_t *flag;
 	kvec_t(uint64_t) stack = {0,0,0};
+	if (n == 0) return;
+	GFA_CALLOC(flag, g->n_seg * 2);
 	for (i = 0; i < n; ++i) {
 		int32_t s;
 		s = gfa_name2id(g, seg[i]);
@@ -20,16 +23,18 @@ void gfa_sub(gfa_t *g, int n, char *const* seg, int step)
 	while (stack.n) {
 		uint64_t x = kv_pop(stack);
 		uint32_t v = x>>32, r = (uint32_t)x;
-		if (g->seg[v>>1].del == 0) continue;
+		if (flag[v]) continue; // already visited
+		flag[v] = 1;
 		g->seg[v>>1].del = 0;
 		if (r < step) {
 			uint32_t nv = gfa_arc_n(g, v);
 			gfa_arc_t *av = gfa_arc_a(g, v);
 			for (i = 0; i < nv; ++i)
-				if (g->seg[av[i].w>>1].del)
+				if (flag[av[i].w] == 0)
 					kv_push(uint64_t, stack, (uint64_t)av[i].w<<32 | (r + 1));
 		}
 	}
 	free(stack.a);
+	free(flag);
 	gfa_arc_rm(g);
 }
