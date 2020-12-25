e563ebe3b060765b2c56920573f238aebdc913f8
Heng Li <lh3@me.com>
2020-12-24 23:40:50 -0500

    First pages commit

---
+++ gfa-plot.js
@@ -0,0 +1,193 @@
+function gfa_plot_arrow(ctx, x, y, len, w, color, rev, text, lw)
+{
+	ctx.font = "10px Arial";
+	if (text != null) {
+		ctx.strokeStyle = "#000000";
+		ctx.textAlign = "center";
+		ctx.fillText(text, x + len/2, y - w - 2);
+	}
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	if (rev == null || !rev) {
+		ctx.lineTo(x - w, y - w);
+		ctx.lineTo(x - w + len, y - w);
+		ctx.lineTo(x + len, y);
+		ctx.lineTo(x - w + len, y + w);
+		ctx.lineTo(x - w, y + w);
+	} else {
+		ctx.lineTo(x + w, y - w);
+		ctx.lineTo(x + w + len, y - w);
+		ctx.lineTo(x + len, y);
+		ctx.lineTo(x + w + len, y + w);
+		ctx.lineTo(x + w, y + w);
+	}
+	ctx.lineTo(x, y);
+	ctx.strokeStyle = color;
+	ctx.lineWidth = lw == null? 1 : lw;
+	ctx.stroke();
+}
+
+function gfa_plot_conf()
+{
+	return {
+		min_len: 10,
+		scale:   10,
+		xskip:   15,
+		yskip:   30
+	};
+}
+
+function gfa_plot_rank2color(g)
+{
+	var color = [ "#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#a65628", "#f781bf" ];
+	var rank = [];
+	for (var i = 0; i < g.seg.length; ++i)
+		if (g.seg[i].rank >= 0) rank.push(g.seg[i].rank);
+	for (var i = 0; i < g.arc.length; ++i)
+		if (g.arc[i].rank >= 0) rank.push(g.arc[i].rank);
+	if (rank.length == 0) return [];
+	rank = rank.sort();
+	var shrink = [rank[0]];
+	for (var i = 1; i < rank.length; ++i)
+		if (rank[i] != rank[i-1])
+			shrink.push(rank[i]);
+	var r2c = {};
+	for (var i = 0; i < shrink.length && i < color.length; ++i)
+		r2c[shrink[i]] = color[i];
+	return r2c;
+}
+
+function gfa_plot_find_v0(g) // FIXME: not general
+{
+	var n_vtx = g.seg.length * 2, v0_ref = -1, v0_src = -1;
+	for (var v = 0; v < n_vtx; ++v) {
+		if (g.seg[v>>1].rank == 0) {
+			if (g.seg[v>>1].snid < 0 || g.seg[v>>1].soff < 0) {
+				info_elem.innerHTML = "Error: a rank-0 segment without stable coordinates";
+				return;
+			}
+			if (v0_ref < 0 || g.seg[v>>1].soff < g.seg[v0_ref>>1].soff)
+				v0_ref = v;
+		}
+		if (g.idx[v^1].n == 0 && v0_src < 0)
+			v0_src = v;
+	}
+	return v0_ref >= 0? v0_ref : v0_src >= 0? v0_src : 0;
+}
+
+function gfa_plot_cal_pos(conf, g)
+{
+	function cal_length(len, min_len, scale) {
+		return Math.floor(min_len + Math.log(len + 1) / Math.log(10) * scale + .499);
+	}
+
+	var v0 = gfa_plot_find_v0(g);
+	var aux = gfa_scc1_aux(g);
+	var sub = gfa_scc1(g, aux, v0);
+	var pred = [];
+	for (var i = 0; i < sub.v.length; ++i) pred[i] = [];
+	for (var i = 0; i < sub.v.length; ++i)
+		for (var j = 0; j < sub.v[i].n; ++j)
+			pred[sub.a[sub.v[i].off + j].i].push(i);
+
+	var l = 0, level_max = [], pos = [];
+	for (var i = 0; i < sub.v.length; ++i) {
+		pos[i] = { level:-1, start:-1, len:0 };
+		pos[i].len = cal_length(g.seg[sub.v[i].v>>1].len, conf.min_len, conf.scale);
+	}
+	for (var i = 0; i < sub.v.length; ++i) {
+		var t = sub.v[i];
+		if (pred[i].length == 0) { // a source node
+			pos[i].start = 0;
+			pos[i].level = level_max.length;
+		} else {
+			var max_end = -1, pl = [];
+			for (var j = 0; j < level_max.length; ++j) pl[j] = { cnt:0, i:-1 };
+			for (var j = 0; j < pred[i].length; ++j) {
+				var pos_ij = pos[pred[i][j]];
+				var end = pos_ij.start + pos_ij.len;
+				if (end > max_end) max_end = end;
+				pl[pos_ij.level].i = pred[i][j];
+				pl[pos_ij.level].end = pos[pred[i][j]].start + pos[pred[i][j]].len;
+				++pl[pos_ij.level].cnt;
+			}
+			pos[i].start = max_end + conf.xskip;
+			// look for an existing level
+			var l;
+			for (l = 0; l < level_max.length; ++l) {
+				if (pl[l].cnt > 1 || level_max[l] + conf.xskip > pos[i].start) continue;
+				if (pl[l].cnt == 0) break;
+				if (pl[l].end == level_max[l]) break;
+			}
+			pos[i].level = l;
+		}
+		level_max[pos[i].level] = pos[i].start + pos[i].len;
+	}
+	return [sub, pos];
+}
+
+function gfa_plot_draw(canvas, conf, g)
+{
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	var ret = gfa_plot_cal_pos(conf, g);
+	var sub = ret[0], pos = ret[1];
+
+	for (var i = 0; i < pos.length; ++i) {
+		pos[i].cx_st = conf.xskip + pos[i].start;
+		pos[i].cx_en = pos[i].cx_st + pos[i].len;
+		pos[i].cy = conf.yskip * (pos[i].level + 1);
+	}
+
+	var max_w = 0, max_h = 0;
+	for (var i = 0; i < pos.length; ++i) {
+		var x = pos[i].cx_en + conf.xskip;
+		max_w = max_w > x? max_w : x;
+		var y = pos[i].cy + conf.yskip;
+		max_h = max_h > y? max_h : y;
+	}
+	canvas.width = max_w, canvas.height = max_h;
+
+	ctx = canvas.getContext("2d");
+	ctx.translate(0.5, 0.5);
+
+	var r2c = gfa_plot_rank2color(g);
+
+	// draw edges
+	ctx.globalAlpha = 0.8;
+	for (var i = 0; i < pos.length; ++i) {
+		for (var j = 0; j < sub.v[i].n; ++j) {
+			ctx.beginPath();
+			ctx.moveTo(pos[i].cx_en, pos[i].cy);
+			var k = sub.a[sub.v[i].off + j].i;
+			ctx.lineTo(pos[k].cx_st, pos[k].cy);
+			var r = sub.a[sub.v[i].off + j].rank;
+			ctx.strokeStyle = r2c[r] != null? r2c[r] : "#A0A0A0";
+			ctx.stroke();
+		}
+	}
+
+	// draw nodes
+	ctx.globalAlpha = 1.0;
+	for (var i = 0; i < pos.length; ++i) {
+		var s = g.seg[sub.v[i].v>>1];
+		var label;
+		if (s.rank == 0) label = s.sname + ":" + s.soff + ":" + s.len;
+		else label = s.len;
+		label = s.len;
+		var color = r2c[s.rank] != null? r2c[s.rank] : "#000000";
+		gfa_plot_arrow(ctx, pos[i].cx_st, pos[i].cy, pos[i].len, 4, color, sub.v[i].v&1, label, s.rank == 0? 1.5 : 1);
+	}
+}
+
+function gfa_plot(canvas, gfa_text, info_elem)
+{
+	var g = gfa_parse(gfa_text);
+	if (g == null || g.seg.length == 0) {
+		info_elem.innerHTML = "Error: failed to parse GFA";
+		return;
+	}
+	var conf = gfa_plot_conf();
+	gfa_plot_draw(canvas, conf, g);
+}
---
+++ gfa.js
@@ -0,0 +1,200 @@
+function gfa_seg_add(g, name)
+{
+	if (name in g.segname) {
+		return g.segname[name];
+	} else {
+		var sid = g.seg.length;
+		g.segname[name] = sid;
+		g.seg.push({ name:name, len:-1, sname:null, soff:-1, rank:-1 });
+		return sid;
+	}
+}
+
+function gfa_index(g)
+{
+	var n_vtx = g.seg.length * 2;
+	for (var v = 0; v < n_vtx; ++v)
+		g.idx[v] = { o:0, n:0 };
+	g.arc = g.arc.sort(function(a,b) { return a.v - b.v });
+	var st = 0;
+	for (var i = 1; i <= g.arc.length; ++i)
+		if (i == g.arc.length || g.arc[i].v != g.arc[st].v)
+			g.idx[g.arc[st].v] = { o:st, n:i-st }, st = i;
+	for (var v = 0; v < n_vtx; ++v) {
+		var ov = g.idx[v].o;
+		var nv = g.idx[v].n;
+		var i0 = -1, n0 = 0;
+		for (var i = 0; i < nv; ++i)
+			if (g.arc[ov + i].rank == 0)
+				++n0, i0 = i;
+		if (n0 > 1) g.err |= 2;
+		if (i0 > 0) { // then swap [0] and [i0]
+			var tmp = g.arc[ov];
+			g.arc[ov] = g.arc[ov + i0];
+			g.arc[ov + i0] = tmp;
+		}
+	}
+}
+
+function gfa_parse(str)
+{
+	var g = { seg:[], arc:[], segname:{}, idx:[], err:0 };
+	var lines = str.split("\n");
+	var re_cigar = /(\d+)([MIDSN])/g;
+	for (var i = 0; i < lines.length; ++i) {
+		if (lines[i].length < 5) continue;
+		var m, t = lines[i].split("\t");
+		if (t[0] == "S") {
+			var sid = gfa_seg_add(g, t[1]);
+			var s = g.seg[sid];
+			if (t[2] != "*") s.len = t[2].length;
+			for (var j = 3; j < t.length; ++j) {
+				if ((m = /^(LN:i|SN:Z|SO:i|SR:i):(\S+)/.exec(t[j])) == null) continue;
+				if (m[1] == "LN:i") s.len = parseInt(m[2]);
+				else if (m[1] == "SN:Z") s.sname = m[2];
+				else if (m[1] == "SO:i") s.soff = parseInt(m[2]);
+				else if (m[1] == "SR:i") s.rank = parseInt(m[2]);
+			}
+		} else if (t[0] == "L") {
+			if (t.length < 5) continue;
+			if (t[2] != '+' && t[2] != '-') continue;
+			if (t[4] != '+' && t[4] != '-') continue;
+			var sid1 = gfa_seg_add(g, t[1]);
+			var sid2 = gfa_seg_add(g, t[3]);
+			var v = sid1<<1 | (t[2] == '+'? 0 : 1);
+			var w = sid2<<1 | (t[4] == '+'? 0 : 1);
+			var ov = 0, ow = 0, rank = -1;
+			for (var j = 6; j < t.length; ++j) {
+				if ((m = /^(SR:i):(\S+)/.exec(t[j])) == null) continue;
+				rank = parseInt(m[2]);
+			}
+			if (t.length >= 6) {
+				while ((m = re_cigar.exec(t[5])) != null) {
+					if (m[2] == 'M' || m[2] == 'D' || m[2] == 'N') ov += parseInt(m[1]);
+					if (m[2] == 'M' || m[2] == 'I' || m[2] == 'S') ow += parseInt(m[1]);
+				}
+			}
+			g.arc.push({ v:v, w:w, ov:ov, ow:ow, rank:rank, ori:true });
+			g.arc.push({ v:w^1, w:v^1, ov:ow, ow:ov, rank:rank, ori:false });
+		}
+	}
+
+	for (var i = 0; i < g.seg.length; ++i)
+		if (g.seg[i].len < 0) g.err |= 1;
+	gfa_index(g);
+	return g;
+}
+
+function gfa_scc1_aux(g)
+{
+	var n_vtx = g.seg.length * 2;
+	var aux = { a:[], index:0 };
+	for (var i = 0; i < n_vtx; ++i)
+		aux.a.push({ index:-1, start:-1, low:0, i:-1, stack:false });
+	return aux;
+}
+
+function gfa_scc1(g, aux, v0)
+{
+	var sub = { v:[], a:[] };
+	var ds = [], ts = [];
+	ds.push([v0, 0]);
+	while (ds.length > 0) {
+		var x = ds.pop();
+		var v = x[0], i = x[1];
+		if (i == 0) { // i is the number of outgoing edges already visited
+			aux.a[v].low = aux.a[v].index = aux.index++;
+			aux.a[v].stack = true;
+			ts.push(v);
+		}
+		var nv = g.idx[v].n;
+		if (i == nv) { // done with v
+			if (aux.a[v].low == aux.a[v].index) {
+				while (ts.length > 0) {
+					var w = ts.pop();
+					sub.v.push({ v:w, off:0, n:0 });
+					aux.a[w].stack = false;
+					if (w == v) break;
+				}
+			}
+			if (ds.length > 0) { // if the DFS stack is not empty, update the top element
+				var w = v;
+				v = ds[ds.length - 1][0];
+				aux.a[v].low = aux.a[v].low < aux.a[w].low? aux.a[v].low : aux.a[w].low;
+			}
+		} else { // process v's neighbor av[i].w
+			var w = g.arc[g.idx[v].o + i].w;
+			ds.push([v, i + 1]);
+			if (aux.a[w].index == -1 && aux.a[w^1].stack == false)
+				ds.push([w, 0]);
+			else if (aux.a[w].stack)
+				aux.a[v].low = aux.a[v].low < aux.a[w].index? aux.a[v].low : aux.a[w].index;
+		}
+	}
+	// reverse the sub.v[] array
+	for (var k = 0; k < sub.v.length>>1; ++k) {
+		var x = sub.v[k];
+		sub.v[k] = sub.v[sub.v.length - 1 - k];
+		sub.v[sub.v.length - 1 - k] = x;
+	}
+	// fill other fields in sub
+	for (var k = 0; k < sub.v.length; ++k)
+		aux.a[sub.v[k].v].start = v0, aux.a[sub.v[k].v].i = k;
+	for (var k = 0; k < sub.v.length; ++k) {
+		var o0 = sub.a.length, v = sub.v[k].v, nv = g.idx[v].n, ov = g.idx[v].o;
+		for (var i = 0; i < nv; ++i) {
+			var a = g.arc[ov + i];
+			if (aux.a[a.w].start == v0)
+				sub.a.push({ i:aux.a[a.w].i, arc_off:ov+i, rank:a.rank });
+		}
+		sub.v[k].off = o0;
+		sub.v[k].n = sub.a.length - o0;
+		if (sub.v[k].n > 1) { // sort
+			var b = sub.a.slice(o0).sort(function(x,y) { return x[0] - y[0] });
+			for (var i = 0; i < b.length; ++i)
+				sub.a[o0 + i] = b[i];
+		}
+	}
+	return sub;
+}
+
+function gfa_scc1_string(g, sub)
+{
+	var lines = [];
+	for (var i = 0; i < sub.v.length; ++i) {
+		var v = sub.v[i].v;
+		var t = ["[" + i + "]", v];
+		t.push("><"[v&1] + g.seg[v>>1].name, sub.v[i].n);
+		if (sub.v[i].n > 0) {
+			var s = [];
+			for (var j = 0; j < sub.v[i].n; ++j)
+				s.push(sub.a[sub.v[i].off + j].i);
+			t.push(s.join(","));
+		}
+		lines.push(t.join("\t"));
+	}
+	return lines.join("\n");
+}
+
+function gfa_string(g)
+{
+	var lines = [];
+	for (var i = 0; i < g.seg.length; ++i) {
+		var s = g.seg[i], t = ['S', s.name, '*'];
+		if (s.len >= 0) t.push('LN:i:' + s.len);
+		if (s.sname != null && s.soff >= 0)
+			t.push('SN:Z:' + s.sname, 'SO:i:' + s.soff);
+		if (s.rank >= 0) t.push('SR:i:' + s.rank);
+		lines.push(t.join("\t"));
+	}
+	for (var i = 0; i < g.arc.length; ++i) {
+		var a = g.arc[i];
+		if (!a.ori) continue;
+		var t = ['L', g.seg[a.v>>1].name, a.v&1? '-' : '+', g.seg[a.w>>1].name, a.w&1? '-' : '+'];
+		if (a.ov == 0 && a.ow == 0) t.push('0M');
+		else t.push(a.ov + ':' + a.ow);
+		if (a.rank >= 0) t.push('SR:i:' + a.rank);
+		lines.push(t.join("\t"));
+	}
+	return lines.join("\n");
+}
