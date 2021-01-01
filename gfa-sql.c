76c4d42b874b4f448bf8233771e070d3ecb8d6c7
Heng Li <lh3@me.com>
2020-12-31 19:13:40 -0500

    r216: added the call table
    Import is implemented in minigraph/misc/mgutils.js bed2sql

--- gfa-sql.c
+++ gfa-sql.c
@@ -50,16 +50,36 @@ CREATE TABLE b2v (           -- bubble-vertex relationship\n\
   bid     INTEGER NOT NULL,  -- bubble ID\n\
   vid     INTEGER NOT NULL   -- vertex ID\n\
 );\n\
+CREATE TABLE bwalk (         -- walk in each bubble\n\
+  wid     INTEGER,           -- walk ID\n\
+  bid     INTEGER NOT NULL,  -- bubble ID\n\
+  len     INTEGER NOT NULL,  -- walk length\n\
+  walk    TEXT NOT NULL,     -- walk\n\
+  PRIMARY KEY (wid)\n\
+);\n\
+CREATE TABLE call (\n\
+  bid     INTEGER,           -- bubble ID\n\
+  sample  TEXT,              -- sample name\n\
+  wid     INTEGER NOT NULL,  -- walk ID\n\
+  ctg     TEXT NOT NULL,     -- sample contig name\n\
+  start   INTEGER NOT NULL,  -- contig start (BED-like)\n\
+  end     INTEGER NOT NULL,  -- contig end\n\
+  strand  INTEGER NOT NULL,  -- contig strand\n\
+  PRIMARY KEY (bid, sample)\n\
+);\n\
 ";
 
 const char *gfa_sql_index = "\
-CREATE UNIQUE INDEX idx_name  ON seg (name);\n\
-CREATE        INDEX idx_scoor ON seg (sname, soff);\n\
-CREATE UNIQUE INDEX idx_s2v   ON vtx (sid, strand);\n\
-CREATE UNIQUE INDEX idx_bst   ON bbl (sname, start);\n\
-CREATE UNIQUE INDEX idx_ben   ON bbl (sname, end);\n\
-CREATE        INDEX idx_b2v   ON b2v (bid, vid);\n\
-CREATE        INDEX idx_v2b   ON b2v (vid);\n\
+CREATE UNIQUE INDEX idx_name  ON seg   (name);\n\
+CREATE        INDEX idx_scoor ON seg   (sname, soff);\n\
+CREATE UNIQUE INDEX idx_s2v   ON vtx   (sid, strand);\n\
+CREATE UNIQUE INDEX idx_bst   ON bbl   (sname, start);\n\
+CREATE UNIQUE INDEX idx_ben   ON bbl   (sname, end);\n\
+CREATE        INDEX idx_b2v   ON b2v   (bid, vid);\n\
+CREATE        INDEX idx_v2b   ON b2v   (vid);\n\
+CREATE        INDEX idx_bwalk ON bwalk (bid);\n\
+CREATE        INDEX idx_cst   ON call  (ctg, start);\n\
+CREATE        INDEX idx_cen   ON call  (ctg, end);\n\
 ";
 
 const char *gfa_sql_view = "\
@@ -68,10 +88,14 @@ CREATE VIEW named_arc AS\n\
   SELECT a.vid1, v1.sid AS sid1, s1.name AS name1, v1.strand AS strand1, a.vid2, v2.sid AS sid2, s2.name AS name2, v2.strand AS strand2, a.ori, a.rank\n\
     FROM arc a, vtx v1, vtx v2, seg s1, seg s2\n\
     WHERE a.vid1 = v1.vid AND a.vid2 = v2.vid AND v1.sid = s1.sid AND v2.sid = s2.sid;\n\
+CREATE VIEW var_call AS\n\
+  SELECT b.bid, b.sname, b.start, b.end, c.sample, c.wid, w.len, c.strand, c.ctg, c.start AS ctg_start, c.end AS ctg_end\n\
+    FROM bbl b, call c, bwalk w WHERE c.bid=b.bid AND c.wid = w.wid AND c.bid = w.bid ORDER BY c.bid;\n\
 ";
 
 // WITH x AS (SELECT t.vid FROM bbl b, b2v t WHERE b.sname="chr11" AND b.end>=10000000 AND b.start<=11000000 AND b.bid=t.bid) SELECT v.* FROM named_vtx v WHERE v.vid IN x;
 // WITH x AS (SELECT t.vid FROM bbl b, b2v t WHERE b.sname="chr11" AND b.end>=10000000 AND b.start<=11000000 AND b.bid=t.bid) SELECT a.* FROM named_arc a WHERE a.vid1 IN x AND a.vid2 IN x;
+// SELECT c.bid, c.sname, c.start, c.end, c.len, COUNT(c.wid) FROM var_call c WHERE c.sname="chr11" AND c.end>=10000000 AND c.start<=11000000 group by c.bid, c.wid;
 
 static inline void str_enlarge(kstring_t *s, int l)
 {
